ca861f5b93e07d2ca25a2c3e9c6c94a4eb8ca308
3djc <Th1sisfmyofmyo>
2020-10-16 16:35:31 +0200

    Allow R9M 2019 in ACCESS mode on some radios (#7963)
    Support for external R9M ACCESS

--- radio/src/trainer.cpp
+++ radio/src/trainer.cpp
@@ -139,8 +139,13 @@ void checkTrainerSettings()
       stop_intmodule_heartbeat();
     else
       init_intmodule_heartbeat();
+#else
+#if defined(HARDWARE_EXTERNAL_ACCESS_MOD)
+    if (g_model.moduleData[EXTERNAL_MODULE].type != MODULE_TYPE_R9M_PXX2) // externalaccessmod 'bridges' HB and Ext module RX pins
+      init_intmodule_heartbeat();
 #elif defined(INTMODULE_HEARTBEAT_GPIO)
     init_intmodule_heartbeat();
+#endif
 #endif
   }
 }
